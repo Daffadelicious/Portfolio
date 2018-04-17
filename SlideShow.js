7c3a699d8efea5458408c820eb05e23ac2703b67
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2018-04-17 14:19:18 -0600

    Add files via upload

---
+++ SlideShow.js
@@ -0,0 +1,16 @@
+// SlideShow   Code from: https://www.w3schools.com/w3css/w3css_slideshow.asp
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);    
+}
